600b2d0688d684768d3846a3d8f50e85919c3435
Suryanshu Patnaik <er.suryanshupatnaik.4@gmail.com>
2022-12-12 22:13:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function alert_1(){
+    alert("Verification of Thevenin's Theorem using Independent source")
+}
+function alert_2(){
+    alert("Verification of Thevenin's Theorem using Dependent source")
+}
